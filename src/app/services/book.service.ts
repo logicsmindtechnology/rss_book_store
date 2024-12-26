62244502b92bef1b68867546ab95bff7ae9657a4
yogeshdotnet@gmail.com <yogeshdotnet@gmail.com>
2024-12-26 19:02:24 +0530

    Now I am using Render.com Node API- Nodejs project hosted at Render

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -8,8 +8,8 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class BookService {
   //private apiUrl = 'http://localhost:8080/books'; // Replace with your API URL
-  private apiUrl ='https://book-store-api-b3faapeagjdchxgg.centralindia-01.azurewebsites.net/books'
-
+  //private apiUrl ='https://book-store-api-b3faapeagjdchxgg.centralindia-01.azurewebsites.net/books'
+private apiUrl = 'https://rss-book-api.onrender.com/books';
   // Observable to share book data
   private booksSubject = new BehaviorSubject<any[]>([]);
   books$ = this.booksSubject.asObservable();
